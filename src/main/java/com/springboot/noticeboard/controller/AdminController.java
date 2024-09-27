35b9028dc5bcf870e1ab0ba54f8a48dcc189ba8a
SeokSeungMin <seoksmin@gmail.com>
2024-09-27 18:12:13 +0900

    Board Post, PostUpdate, PostDelete implement GlobalExceptionHandler added PostEntity, Comment Entity added

--- src/main/java/com/springboot/noticeboard/controller/AdminController.java
+++ src/main/java/com/springboot/noticeboard/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+    //관리자 컨트롤러 작성 예정.
     @GetMapping("/admin")
     public String admin() {
         return "Admin Controller";
